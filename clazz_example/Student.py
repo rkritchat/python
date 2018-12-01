3aa2c78372f2cdc295aad5c7e8a90a2f19ffa008
rkritchat <rkritchat@gmail.com>
2018-12-01 23:10:30 +0700

    Create example for class/object and how to import and use

---
+++ clazz_example/Student.py
@@ -0,0 +1,13 @@
+class Student:
+    def __init__(self, name, last_name):
+        self.name = name
+        self.last_name = last_name
+
+    def show_name(self):
+        print(self.name)
+
+    def show_last_name(self):
+        print(self.last_name)
+
+
+
---
+++ clazz_example/main.py
@@ -0,0 +1,6 @@
+from clazz_example.Student import Student
+
+student = Student('Kritchat', 'Rojanaphruk')
+student.show_name()
+student.show_last_name()
+
