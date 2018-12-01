f403f352edf41d7ec7852ac0364c40729c1dcb2a
rkritchat <rkritchat@gmail.com>
2018-12-01 14:58:50 +0700

    Create example for write/read file in Python

---
+++ read_file.py
@@ -0,0 +1,3 @@
+path = "/Users/laphatrada/desktop/append.txt"
+f = open(path, "r")         # 'r' is read
+print(f.read())             # This will return text inside file append
---
+++ write_file.py
@@ -0,0 +1,7 @@
+file = open("/Users/laphatrada/desktop/append.txt", "a")  # a is append
+file.write("earl\n")                                    # add earl and put new line
+
+
+f = open("/Users/laphatrada/desktop/replace.txt", "w")     # w is new file[ replace if exist
+f.write("Away replace old letter")
+
